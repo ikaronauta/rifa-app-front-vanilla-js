5de86ba20447f7977f77c88613427d46a816c2d5
ikaronauta <ortizgalindoj@gmail.com>
2025-03-31 20:51:53 -0500

    ajuste base URL

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const API_KEY = "4Vj8eK4rloUd272L48hsrarnUA";
 const merchantId = '508029';
 const accountId = '512321';
 const currency = 'COP';
-const baseUrl = 'https://8e5c-191-156-33-176.ngrok-free.app';
+const baseUrl = 'https://ca13-191-156-47-190.ngrok-free.app';
 const confirmationUrl = `${baseUrl}/payu-confirmation`;
 
 let boletas, tabla;
